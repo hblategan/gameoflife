076324304517b0cc68ee26a2dd37901b79fdf4d1
Herko Lategan <herko@impactradius.com>
2019-02-16 09:59:08 +0200

    git ignore initial

---
+++ src/test/kotlin/com/stellenbosch/MainTest.kt
@@ -0,0 +1,12 @@
+package com.stellenbosch
+
+import kotlin.test.assertEquals
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class TestSource {
+    @Test
+    fun f() {
+        assertTrue(true)
+    }
+}
